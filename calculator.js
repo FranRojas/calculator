197e66d33d04a8e15a07ffd85db5842147ca79de
FrancisRojas <francisjr@pop-os.localdomain>
2022-06-02 16:53:01 -0400

    more functions

--- calculator.js
+++ calculator.js
@@ -1,5 +1,37 @@
+const screen = document.querySelector(".Screen")
+
+const btns = document.querySelectorAll("button")
+let a;
+let b;
+let operator;
+let result;
+let internalbuffer;
+let screenBuffer;
+screen.textContent="0"
+
+btns.forEach(button =>button.addEventListener("click", (e)=>{
+    console.log(e.target.textContent)
+    screenBuffer=e.target.textContent;
+
+    
+}) )
+function populate(value){
+    screen.textContent = value
+}
+
+
+
+function eventHandler(value){
+  
+}
+
+
+
+
 function add(a,b){
-    return a + b
+    result =a +b;
+    screen.textContent = result
+    return result
 }
 
 function subtract(a,b){
@@ -15,7 +47,7 @@ function divide(a,b){
 
 function operate(operator, a, b){
     switch (operator){
-        case "+": add(a,b);
+        case "+": return add(a,b);
         case "-" : return subtract(a,b);
       	case "*" : return multiply(a,b);
       	case "/" : return divide(a,b);
